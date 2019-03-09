2cc5888963a6129868bbc58d66e0f8146a55aaed
TamanegiMage <31086738+TamanegiMage@users.noreply.github.com>
2019-03-09 15:20:59 +0900

    version 1.13.2.0

--- EnmityPlugin/Properties/AssemblyInfo.cs
+++ EnmityPlugin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Enmity Plugin")]
-[assembly: AssemblyCopyright("Copyright © Tomonori Tamagawa, TamanegiMage 2015-2018")]
+[assembly: AssemblyCopyright("Copyright © Tomonori Tamagawa, TamanegiMage 2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.13.1.0")]
-[assembly: AssemblyFileVersion("1.13.1.0")]
+[assembly: AssemblyVersion("1.13.2.0")]
+[assembly: AssemblyFileVersion("1.13.2.0")]
